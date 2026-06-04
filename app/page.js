c3b160385d6c4608d547d839d5d1c4ff10fd90fc
ferdyansyahwal3-sy <ferdyansyahwal3@gmail.com>
2026-06-04 16:13:13 +0700

    feat: cta section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
+import Works from '@/components/Works'
+import Skills from '@/components/Skills'
+import Testimonials from '@/components/Testimonials'
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <Works />
+      <Skills />
+      <Testimonials />
     </main>
   )
 }
